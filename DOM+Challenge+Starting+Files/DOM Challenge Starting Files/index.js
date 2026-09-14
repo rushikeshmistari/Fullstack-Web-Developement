97ada54fc1ecb0d675cafbc5bc743503bf56dd45
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-14 14:32:48 +0530

    Auto push: index.js

--- DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
+++ DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -1,4 +1,4 @@
-var el = document .querySelector("#list a");
-el.style.color = "red";
+// var el = document .querySelector("#list a");
+// el.style.color = "red";
 var butto = document.querySelector("button");
 butto.style.backgroundColor = "Yellow";
