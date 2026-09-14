e7922e915754c69b6ebd094a79c7f737d0f8abb6
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-14 22:32:56 +0530

    Auto push: index.js

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -11,14 +11,14 @@ var num2 = Math.floor(Math.random() * 6);
 
 if (num1 > num2) {
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
-    document.querySelector(".img1").src = img[num1];
-    document.querySelector(".img2").src = img[num2];
+    document.querySelector(".img1").setAttribute("src", img[num1]);
+    document.querySelector(".img2").setAttribute("src", img[num2]);
 } else if (num2 > num1) {
     document.querySelector("h1").innerHTML = "Player 2 Wins!";
-    document.querySelector(".img2").src = img[num2];
-    document.querySelector(".img1").src = img[num1];
+    document.querySelector(".img2").setAttribute("src", img[num2]);
+    document.querySelector(".img1").setAttribute("src", img[num1]);
 } else {
     document.querySelector("h1").innerHTML = "It's a Draw!";
-    document.querySelector(".img1").src = img[num1];
-    document.querySelector(".img2").src = img[num2];
+    document.querySelector(".img1").setAttribute("src", img[num1]);
+    document.querySelector(".img2").setAttribute("src", img[num2]);
 };
