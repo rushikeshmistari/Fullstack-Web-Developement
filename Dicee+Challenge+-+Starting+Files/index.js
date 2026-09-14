0c59957f9973d3e6896804254d26fb5904d399e0
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-14 22:31:19 +0530

    Auto push: index.js

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,24 +1,24 @@
-// var img1 = "D:\Udemy\Dicee+Challenge+-+Starting+Files\images\dice1.png";
-// var img2 = "D:\Udemy\Dicee+Challenge+-+Starting+Files\images\dice2.png";
-// var img3 = "D:\Udemy\Dicee+Challenge+-+Starting+Files\images\dice3.png";
-// var img4 = "D:\Udemy\Dicee+Challenge+-+Starting+Files\images\dice4.png";
-// var img5 = "D:\Udemy\Dicee+Challenge+-+Starting+Files\images\dice5.png";
-// var img6 = "D:\Udemy\Dicee+Challenge+-+Starting+Files\images\dice6.png";
-// var img = [img1, img2, img3, img4, img5, img6];
+var img1 = "images\dice1.png";
+var img2 = "images\dice2.png";
+var img3 = "images\dice3.png";
+var img4 = "images\dice4.png";
+var img5 = "images\dice5.png";
+var img6 = "images\dice6.png";
+var img = [img1, img2, img3, img4, img5, img6];
 
-// var num1 = Math.floor(Math.random() * 6);
-// var num2 = Math.floor(Math.random() * 6);
+var num1 = Math.floor(Math.random() * 6);
+var num2 = Math.floor(Math.random() * 6);
 
-// if (num1 > num2) {
-//     document.querySelector("h1").innerHTML = "Player 1 Wins!";
-//     document.querySelector(".img1").src = img[num1];
-//     document.querySelector(".img2").src = img[num2];
-// } else if (num2 > num1) {
-//     document.querySelector("h1").innerHTML = "Player 2 Wins!";
-//     document.querySelector(".img2").src = img[num2];
-//     document.querySelector(".img1").src = img[num1];
-// } else {
-//     document.querySelector("h1").innerHTML = "It's a Draw!";
-//     document.querySelector(".img1").src = img[num1];
-//     document.querySelector(".img2").src = img[num2];
-// };
+if (num1 > num2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    document.querySelector(".img1").src = img[num1];
+    document.querySelector(".img2").src = img[num2];
+} else if (num2 > num1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    document.querySelector(".img2").src = img[num2];
+    document.querySelector(".img1").src = img[num1];
+} else {
+    document.querySelector("h1").innerHTML = "It's a Draw!";
+    document.querySelector(".img1").src = img[num1];
+    document.querySelector(".img2").src = img[num2];
+};
