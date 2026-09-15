7792b326078156b8b6bcfdbbc46ff89fdec992ca
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-15 15:48:26 +0530

    Auto push: index.js

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,24 +1,28 @@
-
-// document.querySelector(".s.drum").addEventListener("click", function() {
-//     alert("You clicked button s !!!");
-// });
-// document.querySelector(".d.drum").addEventListener("click", function() {
-//     alert("You clicked button d!!!");
-// });
-// document.querySelector(".j.drum").addEventListener("click", function() {
-//     alert("You clicked button j!!!");
-// });
-// document.querySelector(".k.drum").addEventListener("click", function() {
-//     alert("You clicked button k!!!");
-// });
-// document.querySelector(".l.drum").addEventListener("click", function() {
-//     alert("You clicked button l!!!");
-// });
 document.querySelector(".w.drum").addEventListener("click", function() {
     var audio = new Audio("sounds/tom-1.mp3");
     audio.play();
 });
 document.querySelector(".a.drum").addEventListener("click", function(){
     var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+});
+document.querySelector(".s.drum").addEventListener("click", function() {
+    var audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+});
+document.querySelector(".d.drum").addEventListener("click", function() {
+    var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+});
+document.querySelector(".j.drum").addEventListener("click", function() {
+    var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+});
+document.querySelector(".k.drum").addEventListener("click", function() {
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+});
+document.querySelector(".l.drum").addEventListener("click", function() {
+    var audio = new Audio("sounds/kick-bass.mp3");
     audio.play();
 });
