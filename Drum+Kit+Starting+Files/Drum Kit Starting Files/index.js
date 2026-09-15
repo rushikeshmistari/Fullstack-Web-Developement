233470fb72c97d08d85885520dc2ae5f384fbc43
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-15 16:30:37 +0530

    Auto push: index.js

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -5,37 +5,30 @@ for (var i = 0; i < document.querySelector(".button").clientHeight; i++) {
             case "w":
                 var audio = new Audio("sounds/tom-1.mp3");
                 audio.play();
-                document.querySelector(".w.drum").style.color = "white";
                 break;
             case "a":
                 var audio = new Audio("sounds/tom-2.mp3");
                 audio.play();
-                document.querySelector(".a.drum").style.color = "white";
                 break;
             case "s":
                 var audio = new Audio("sounds/tom-3.mp3");
                 audio.play();
-                document.querySelector(".s.drum").style.color = "white";
                 break;
             case "d":
                 var audio = new Audio("sounds/tom-4.mp3");
                 audio.play();
-                document.querySelector(".d.drum").style.color = "white";
                 break;
             case "j":
                 var audio = new Audio("sounds/snare.mp3");
                 audio.play();
-                document.querySelector(".j.drum").style.color = "white";
                 break;
             case "k":
                 var audio = new Audio("sounds/crash.mp3");
                 audio.play();
-                document.querySelector(".k.drum").style.color = "white";
                 break;
             case "l":
                 var audio = new Audio("sounds/kick-bass.mp3");
                 audio.play();
-                document.querySelector(".l.drum").style.color = "white";
                 break;
             default:
                 console.log(buttonInnerHTML);
