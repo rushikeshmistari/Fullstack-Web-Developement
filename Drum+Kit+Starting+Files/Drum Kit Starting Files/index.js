b6174dd6100f2d4d624f5701f3f4d6c3c46f2145
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-15 10:59:55 +0530

    Auto push: index.js

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,5 +1,21 @@
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked!");
+document.querySelector(".w.drum").addEventListener("click",function() {
+    alert("I got Clicked button w !!!");
 });
-}
+document.querySelector(".a.drum").addEventListener("click", function(){
+    alert("You clicked button a !!!");
+});
+document.querySelector(".s.drum").addEventListener("click", function() {
+    alert("You clicked button s !!!");
+});
+document.querySelector(".d.drum").addEventListener("click", function() {
+    alert("You clicked button d!!!");
+});
+document.querySelector(".j.drum").addEventListener("click", function() {
+    alert("You clicked button j!!!");
+});
+document.querySelector(".k.drum").addEventListener("click", function() {
+    alert("You clicked button k!!!");
+});
+document.querySelector(".l.drum").addEventListener("click", function() {
+    alert("You clicked button l!!!");
+});
