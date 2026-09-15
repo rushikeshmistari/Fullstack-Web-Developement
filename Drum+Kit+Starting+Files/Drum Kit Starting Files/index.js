3789db1bfdd4e2560e3774d442d06fd2a235de88
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-15 10:43:32 +0530

    Auto push: index.js

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,10 +1,4 @@
 var func = function () {
     alert("I got clicked!");
 };
-document.querySelector(".w").addEventListener("click", func);
-document.querySelector(".a").addEventListener("click", func);
-document.querySelector(".s").addEventListener("click", func);
-document.querySelector(".d").addEventListener("click", func);
-document.querySelector(".j").addEventListener("click", func);
-document.querySelector(".k").addEventListener("click", func);
-document.querySelector(".l").addEventListener("click", func);
+document.querySelectorAll("button")[0].addEventListener("click", func);
