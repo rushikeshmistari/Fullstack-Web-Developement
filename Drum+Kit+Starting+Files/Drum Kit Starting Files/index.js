101b1b9f7fc3a2fa9de8f58e922e9e3201005f5f
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-15 15:43:04 +0530

    Auto push: index.js

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,21 +1,25 @@
-document.querySelector(".w.drum").addEventListener("click",function() {
-    alert("I got Clicked button w !!!");
-});
-document.querySelector(".a.drum").addEventListener("click", function(){
-    alert("You clicked button a !!!");
-});
-document.querySelector(".s.drum").addEventListener("click", function() {
-    alert("You clicked button s !!!");
-});
-document.querySelector(".d.drum").addEventListener("click", function() {
-    alert("You clicked button d!!!");
-});
-document.querySelector(".j.drum").addEventListener("click", function() {
-    alert("You clicked button j!!!");
-});
-document.querySelector(".k.drum").addEventListener("click", function() {
-    alert("You clicked button k!!!");
-});
-document.querySelector(".l.drum").addEventListener("click", function() {
-    alert("You clicked button l!!!");
+// document.querySelector(".w.drum").addEventListener("click",function() {
+//     alert("I got Clicked button w !!!");
+// });
+// document.querySelector(".a.drum").addEventListener("click", function(){
+//     alert("You clicked button a !!!");
+// });
+// document.querySelector(".s.drum").addEventListener("click", function() {
+//     alert("You clicked button s !!!");
+// });
+// document.querySelector(".d.drum").addEventListener("click", function() {
+//     alert("You clicked button d!!!");
+// });
+// document.querySelector(".j.drum").addEventListener("click", function() {
+//     alert("You clicked button j!!!");
+// });
+// document.querySelector(".k.drum").addEventListener("click", function() {
+//     alert("You clicked button k!!!");
+// });
+// document.querySelector(".l.drum").addEventListener("click", function() {
+//     alert("You clicked button l!!!");
+// });
+document.querySelector(".w.drum").addEventListener("click", function() {
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
 });
