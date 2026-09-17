f9a861a806c562191781ebd03403f1984b8c8b90
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-17 10:50:41 +0530

    Auto push: game.js

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -3,23 +3,22 @@ var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4);
-
-    if (randomNumber === 0) {
-        $("#red").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
-    }
-    else if (randomNumber === 1) {
-        $("#blue").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
-    }
-    else if (randomNumber === 2) {
-        $("#green").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
-    }
-    else if (randomNumber === 3) {
-        $("#yellow").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
-    };
     
 };
+var randomNumber = Math.floor(Math.random() * 4);
 
 var randomChosenColor = buttonColors[randomNumber];
 gamePattern.push(randomChoosenColor);
 
+if (randomNumber === 0) {
+    $("#red").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
+}
+else if (randomNumber === 1) {
+    $("#blue").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
+}
+else if (randomNumber === 2) {
+    $("#green").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
+}
+else if (randomNumber === 3) {
+    $("#yellow").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
+};
