1164a5776afa3b69b54e1003b80e25a38a2b214e
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-17 21:00:51 +0530

    Auto push: index.js

--- Backend/2.3 NPM/index.js
+++ Backend/2.3 NPM/index.js
@@ -1,7 +1,7 @@
-import generateName from 'sillyname';
-// var generateName = require('sillyname');
-var sillyName = generateName();
-console.log(`My silly name is: ${sillyName}`);
+// import generateName from 'sillyname';
+// // var generateName = require('sillyname');
+// var sillyName = generateName();
+// console.log(`My silly name is: ${sillyName}`);
 
 import superheroes from 'superheroes';
 var name = superheroes.random();
