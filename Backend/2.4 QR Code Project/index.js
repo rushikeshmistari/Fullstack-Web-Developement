fdf0e43aadeb2ef8c6252bb2640d4cc263c77b11
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-18 00:08:08 +0530

    Auto push: index.js

--- Backend/2.4 QR Code Project/index.js
+++ Backend/2.4 QR Code Project/index.js
@@ -4,23 +4,44 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
 inquirer
     .prompt([
         {
             type: 'input',
-            url: 'username',
+            name: 'username',
             message: 'What is your url?'
         }
     ])
     .then((answers) => {
+
         console.log('Your url is:', answers.username);
+
+        // Generate QR code
+        var qr_png = qr.image(answers.username, { type: 'png' });
+
+        // Save QR code
+        qr_png.pipe(fs.createWriteStream('qr_img.png'));
+
+        // Save URL in text file
+        fs.writeFile('Username.txt', answers.username, (err) => {
+            if (err) throw err;
+
+            console.log('The file has been saved!');
+        });
+
+        console.log('QR code generated!');
     })
     .catch((error) => {
+
         if (error.isTtyError) {
             console.log('Prompt could not be rendered');
         } else {
             console.log('Something went wrong');
         }
+
     });
 
 import qr from 'qr-image';
