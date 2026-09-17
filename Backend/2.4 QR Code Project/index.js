ecb39e4ce846f03d59cbd03e37524a587515d545
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-17 23:29:22 +0530

    Auto push: index.js

---
+++ Backend/2.4 QR Code Project/index.js
@@ -0,0 +1,25 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+const inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your name?'
+        }
+    ])
+    .then((answers) => {
+        console.log('Your name is:', answers.username);
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            console.log('Prompt could not be rendered');
+        } else {
+            console.log('Something went wrong');
+        }
+    });
