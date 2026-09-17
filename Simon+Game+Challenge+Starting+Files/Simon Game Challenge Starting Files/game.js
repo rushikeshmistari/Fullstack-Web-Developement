2a0f8af006f39b2f4d7042f64046cdfd086fc1d1
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-17 11:19:32 +0530

    Auto push: game.js

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -14,6 +14,6 @@ $(document).keypress(function nextSequence() {
     var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     audio.play();
 
-    var userChosenColor = this.id;
+    var userChosenColor = $(this).attr("id");
     console.log(userChosenColor);
 });
