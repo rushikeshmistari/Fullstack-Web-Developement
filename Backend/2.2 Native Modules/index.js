16b06ab77e67cd216771b17faf13541437e96499
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-17 19:28:04 +0530

    Auto push: index.js

--- Backend/2.2 Native Modules/index.js
+++ Backend/2.2 Native Modules/index.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
 
-writeFile("message.txt","Hello from Rushikesh", (err) => {
+fs.writeFile("message.txt","Hello from Rushikesh", (err) => {
     if (err) throw err;
     console.log("File has been created successfully");
 });
 
+fs.readFile('/etc/passwd', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
