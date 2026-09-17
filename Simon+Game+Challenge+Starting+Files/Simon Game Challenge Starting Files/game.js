8dc94af48fb0986411b7bedd25a39c3d10daee0c
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-17 11:06:10 +0530

    Auto push: game.js

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,15 +1,13 @@
-var gamePattern = [];
-
-var buttonColors = ["red", "blue", "green", "yellow"];
-
 function nextSequence() {
-    
-};
-var randomNumber = Math.floor(Math.random() * 4);
+    level++;
+    $("#level-title").text("Level " + level);
 
-var randomChoosenColor = buttonColors[randomNumber];
-gamePattern.push(randomChoosenColor);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChoosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChoosenColor);
 
-$("#" + randomChoosenColor).fadeOut(200).fadeIn(200);
-var sound = new Audio('sounds/' + randomChoosenColor + '.mp3');
-sound.play();
+    $("#" + randomChoosenColor).fadeOut(200).fadeIn(200);
+
+    var sound = new Audio('sounds/' + randomChoosenColor + '.mp3');
+    sound.play();
+}
