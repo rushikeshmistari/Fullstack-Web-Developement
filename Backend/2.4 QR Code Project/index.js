fc02169cdae8b1f88940461fabaaab2d6ca2bdc0
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-17 23:35:04 +0530

    Auto push: index.js

--- Backend/2.4 QR Code Project/index.js
+++ Backend/2.4 QR Code Project/index.js
@@ -3,8 +3,7 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
-const inquirer = require("inquirer");
-
+import inquirer from 'inquirer';
 inquirer
     .prompt([
         {
