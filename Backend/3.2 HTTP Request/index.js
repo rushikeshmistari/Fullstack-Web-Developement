743ec296d6246eb7c622257dbcfbbe2bd5ebebc3
RushikeshMistari2 <rushikesh.2506747101@vcet.edu.in>
2026-09-24 17:39:10 +0530

    Auto push: index.js

--- Backend/3.2 HTTP Request/index.js
+++ Backend/3.2 HTTP Request/index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port , () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port localhost:${port}`);
 });
 
 
